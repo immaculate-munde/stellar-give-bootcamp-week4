e3d11ce3fde1d18c13b7b8537bd950f0d486b613
Immaculate Munde <immaculatemunde1@gmail.com>
2026-05-28 13:00:12 +0300

    Add initial frontend configuration files
    - Created next.config.mjs to configure image remote patterns for the frontend.
    - Added package.json to define project dependencies and scripts for development, build, and linting.
    - Introduced package-lock.json to lock dependencies for consistent installations.
    - Added postcss.config.mjs to configure PostCSS with Tailwind CSS as a plugin.

---
+++ frontend/next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "files.catbox.moe",
+      },
+      {
+        protocol: "https",
+        hostname: "**.example.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
